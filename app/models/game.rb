337a46c3389aec9a7be9d59fd8ce12947472a856
mauro <mauro_gs@hotmail.com>
2021-05-25 21:24:38 +0200

    Add game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,30 @@ class Game < ApplicationRecord
   belongs_to :player
 
   enum status: { started: 0, finished: 1 }
+
+  validates :status, presence: true, inclusion: { in: Game.statuses.keys }
+  validate :moves_fields
+
+  private
+
+  def moves_fields
+    return unless moves.is_a? Array
+
+    moves.each do |move|
+      valid_move_keys(move.stringify_keys)
+      valid_move_to_play(move.stringify_keys)
+    end
+  end
+
+  def valid_move_keys(move)
+    return if (%w(name move moved_at) - move.keys).empty?
+
+    errors.add(:moves, 'Invalid key')
+  end
+
+  def valid_move_to_play(move)
+    return if MOVES.include?(move['move'])
+
+    errors.add(:moves, 'Invalid move')
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  let(:player) { Player.create(name: 'Alex') }
+  let(:game) { Game.create(player_id: player.id) }
+
+  it 'should create a new game object' do
+    expect(game).to be_a Game
+  end
+
+  it 'should respond to his fields' do
+    expect(game).to respond_to(:status)
+    expect(game).to respond_to(:moves)
+    expect(game).to respond_to(:player_wins)
+    expect(game).to respond_to(:player_id)
+
+    expect(game.status).to eq('started')
+    expect(game.moves).to be_a Array
+    expect(game.player_wins).to be false
+    expect(game.player_id).not_to be_nil
+    expect(game.player_id).to eq(player.id)
+  end
+
+  it 'should save valid statuses' do
+    expect(game.valid?).to be true
+    expect(game.status).to eq('started')
+
+    game.status = 'finished'
+
+    expect(game.valid?).to be true
+    expect(game.status).to eq('finished')
+  end
+
+  it 'should not save moves with invalid keys' do
+    move = {  user: player,
+              move: 'rock',
+              moved_at: Time.zone.now }
+    game.moves = [move]
+
+    expect(game.valid?).to be false
+    expect(game.errors.messages[:moves]).to include('Invalid key')
+  end
+
+  it 'should not save moves with invalid move to play' do
+    move = {  name: 'Bot',
+              move: 'bad movement',
+              moved_at: Time.zone.now }
+    game.moves = [move]
+
+    expect(game.valid?).to be false
+    expect(game.errors.messages[:moves]).to include('Invalid move')
+  end
 end
