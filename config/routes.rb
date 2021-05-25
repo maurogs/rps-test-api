4a325bcf79518dcaeec1bec97f9c21eaf145c946
mauro <mauro_gs@hotmail.com>
2021-05-25 20:35:19 +0200

    Add get and post endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games, only: :index do
+    post 'play', on: :collection
+  end
 end
