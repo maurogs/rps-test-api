ba861b6eaa388436a29712d0ce6a41cba217cb57
mauro <mauro_gs@hotmail.com>
2021-05-25 21:43:31 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,155 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Player.destroy_all
+Game.destroy_all
+
+player_one = Player.create(name: 'Tony')
+player_two = Player.create(name: 'John')
+player_three = Player.create(name: 'Alex')
+player_four = Player.create(name: 'Carl')
+
+moves_one = { 
+              name: player_one.name,
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            }
+Game.create(player_id: player_one.id, status: 1, moves: moves_one, player_wins: false)
+
+moves_two = { 
+              name: player_one.name,
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_two, player_wins: false)
+
+moves_three = { 
+              name: player_two.name,
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            },
+            { 
+              name: player_two.name,
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_three, player_wins: true)
+
+moves_four = { 
+              name: player_three.name,
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            { 
+              name: player_three.name,
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_four, player_wins: false)
+
+moves_five = { 
+              name: player_three.name,
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_five, player_wins: true)
+
+moves_six = { 
+              name: player_three.name,
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_six, player_wins: true)
+
+moves_six = { 
+              name: player_four.name,
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_six, player_wins: false)
+
+moves_seven = { 
+              name: player_four.name,
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            { 
+              name: player_four.name,
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            } ,
+            { 
+              name: player_four.name,
+              move: Game::MOVES[1],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_seven, player_wins: true)
+
+moves_eight = { 
+              name: player_four.name,
+              move: Game::MOVES[2],
+              moved_at: Time.zone.now
+            },
+            {
+              name: 'Bot',
+              move: Game::MOVES[0],
+              moved_at: Time.zone.now
+            }            
+Game.create(player_id: player_one.id, status: 1, moves: moves_eight, player_wins: false)
